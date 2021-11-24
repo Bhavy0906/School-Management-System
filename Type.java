dc1f1ee720aaa5f46401f18aa6b887c92c402114
Uday-Mittal-8860 <udaymittal8860@gmail.com>
2021-11-24 09:12:04 +0530

    Create Type.java

---
+++ Type.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Type {
+
+    public int post ()throws IOException
+    {
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+        Details details = new Details();
+        details.loginDetails();
+        int ch = Integer.parseInt(br.readLine());
+        return ch;
+    }
+    
+}

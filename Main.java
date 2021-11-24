13332ff221262b80b8ca0b99c6edc31d6f3b14e8
Uday-Mittal-8860 <91343270+Uday-Mi@users.noreply.github.com>
2021-11-24 17:55:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,20 +20,30 @@ public class Main {
         //Asks the user whether he wants to log in or sign in the system.
 
         Signup signup = new Signup();
+        //Creates instance of the sign up class
         Login login = new Login();
+        //Creates instance of the login class
         Type type = new Type();
+        //Creates instance of the type class
+        //The class asks whether you are a student or a teacher or admin
 
         int t = 0;
+        //Saves whether the user is a student, or a teacher, or admin
         TreeMap<String, String> tree;
         MapReturn mapReturn = new MapReturn();
+        //Creates instance of the class which returns the tree map of student, teacher or admin
         Input input = new Input();
+        //Takes user ID and password input from the user
         String userID = "";
         String password = "";
         int enter = 0;
+        //Stores whether user wants to signup or login
 
+        Outer:
         do {
             enter = Integer.parseInt(br.readLine());
             t = type.post();
+            //post method defines whether user is a student or a teacher or admin.
             if (t == 1 || t == 2 || t == 3) {
                 tree = mapReturn.type(t);
             } else {
@@ -45,14 +55,25 @@ public class Main {
             if (password != "") {
                 switch (enter) {
                 case 1:
-                    signup.setUserID(userID, password, tree);
-                    break;
+                    if(signup.setUserID(userID, password, tree))
+                        continue;
+                    break Outer;
                 case 2:
-                    login.loginCheck(userID, password, tree);
+                    if(!login.loginCheck(userID, password, tree))                   
+                        break Outer;
                     break;
+                default:
+                    break Outer;
                 }
-            } else
+            } else{
+                System.out.println("Password format incorrect");
+                System.out.println("Try again");
                 break;
+            }
+            if (t == 1)
+            
+
+
         } while (false);
 
     }

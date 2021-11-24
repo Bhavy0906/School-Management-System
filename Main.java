3b4b8bc7e0b8441bbe1fa281d7f63f5896844842
Uday-Mittal-8860 <udaymittal8860@gmail.com>
2021-11-24 09:11:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,30 @@ import java.util.*;
 
 public class Main {
 
-    public void details ()
-    {
-        System.out.println("********************Welcome to the School Management System********************");
-        System.out.println("");
-    }
     public static void main(String[] args)throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
+        Details details = new Details ();  
+        User user = new User();      
+        details.basicDisplay();
+        details.enterInto();
+        int enter = Integer.parseInt(br.readLine());
+        Signup signup = new Signup();
+        Type type = new Type();
+        int t = 0;
+        switch (enter)
+        {
+            case 1:
+            t = type.post();
+            if (t == 1 || t == 2 || t == 3)
+            {
+
+            }
+            else 
+                details.
+            signup.setUserID(userID, password, type)
+        }
 
     }
 }

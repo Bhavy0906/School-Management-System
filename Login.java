2a18db6446cce2c7336af75f72e370ebdf306ee1
Uday-Mittal-8860 <udaymittal8860@gmail.com>
2021-11-24 09:36:59 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,25 +1,23 @@
 import java.io.*;
+import java.util.*;
 
 public class Login extends User{
 
     private String userID;
     private String password;
     static BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
-    
-    boolean loginCheck (String userID, String password)throws IOException
-    {
-        System.out.println("Please enter 1 if you are student, 2 if you are a teacher, and 3 if you are the admin");
-        int choice = Integer.parseInt(br.readLine());
-        String arr[] = {"Student", "Teacher", "Admin"};
-        switch (choice)
-        {
-            case 1:
-            // loginDetails(userID, password, super.student);
 
-        }
-        if (this.userID.equals(userID))
+    public Login ()
+    {
+        userID = "";
+        password = "";
+    }
+    
+    boolean loginCheck (String userID, String password, TreeMap<String, String> type)throws IOException
+    { 
+        if (type.containsKey(userID)) 
         {
-            if (this.password.equals(this.password))
+            if (type.containsValue(password))
                 return true;
             else
                 return false;
@@ -36,11 +34,5 @@ public class Login extends User{
     {
         this.password = password;
     }
-
-    boolean loginDetails (String userID, String password, TreeMap check)
-    {
-        
-    }
-    
     
 }

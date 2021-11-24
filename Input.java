0a1c04c9ccc521e1339ba75e8b0faa32e3b488a0
Uday-Mittal-8860 <udaymittal8860@gmail.com>
2021-11-24 09:28:35 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+public class Input {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public String userID ()throws IOException
+    {
+        System.out.println("Enter the user id");
+        return br.readLine();
+    }
+
+    public String password ()throws IOException
+    {
+        String pass = br.readLine();    
+        boolean valid = passwordCheck(pass);
+        if (valid)
+            return pass;
+        return "";    
+    }
+    
+    public void passwordDetails ()
+    {
+        System.out.println("Enter the password"); 
+        System.out.println("The password should be atleast 8 characters long and the maximum length of the password should be 15 characters");
+        System.out.println("The password should contain atleast one uppercase letter, one lowercase letter, and one numeric character");
+    }
+
+    public boolean passwordCheck (String pass)
+    {
+        boolean capital = false;
+        boolean small = false;
+        boolean number = false;
+
+        for (int i = 0; i < pass.length(); i++)
+        {
+            char ch = pass.charAt(i);
+            if (Character.isLowerCase(ch))
+                small = true;
+            if (Character.isUpperCase(ch))
+                capital = true;
+            if (Character.isDigit(ch))
+                number = true;
+            if (capital && small && number)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}

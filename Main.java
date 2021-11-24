c5432a7db931d301dbdec6cd377057f522402ecd
Uday-Mittal-8860 <91343270+Uday-Mi@users.noreply.github.com>
2021-11-24 14:03:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,51 +3,57 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args)throws IOException {
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
-        Details details = new Details ();  
-        User user = new User();      
+
+        Details details = new Details();
+        //Details class displays the basic information and various print statements guiding the user through the code.
+
+        User user = new User();
+        //Contains information about about the school
+
         details.basicDisplay();
+        //Displays the front page of the school
         details.enterInto();
-        int enter = Integer.parseInt(br.readLine());
+        //Asks the user whether he wants to log in or sign in the system.
+
         Signup signup = new Signup();
-        Login login = new Login ();
+        Login login = new Login();
         Type type = new Type();
+
         int t = 0;
-        TreeMap <String, String> tree;
+        TreeMap<String, String> tree;
         MapReturn mapReturn = new MapReturn();
         Input input = new Input();
         String userID = "";
         String password = "";
-        t = type.post();
-        if (t == 1 || t == 2 || t == 3)
-        {
-            tree = mapReturn.type(t);
-        }
-        else 
-        {
-            details.invalidLoginDetails();
-            break;
-        }    
-        userID = input.userID();
-        password = input.password();
-        if (password != "")
-        {
-            switch (enter)
-            {
-                case 1:
-                signup.setUserID(userID, password, tree);
-                break;
+        int enter = 0;
 
-                case 2:
-                login.loginCheck(userID, password, tree);
+        do {
+            enter = Integer.parseInt(br.readLine());
+            t = type.post();
+            if (t == 1 || t == 2 || t == 3) {
+                tree = mapReturn.type(t);
+            } else {
+                details.invalidLoginDetails();
                 break;
             }
-        }
-        else
-            break;
+            userID = input.userID();
+            password = input.password();
+            if (password != "") {
+                switch (enter) {
+                case 1:
+                    signup.setUserID(userID, password, tree);
+                    break;
+                case 2:
+                    login.loginCheck(userID, password, tree);
+                    break;
+                }
+            } else
+                break;
+        } while (false);
 
     }
 }

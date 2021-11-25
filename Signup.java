d6485f09c4b3ac36beabb58ae15ee89c828a5ce1
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 10:14:37 +0530

    Update Signup.java

--- Signup.java
+++ Signup.java
@@ -2,27 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Signup extends User {
-
-    private String userID;
-    private String password;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String userID;
+    String password;
+    String userName;
 
-    public Signup ()
-    {
-        userID = "";
-        password = "";
+    public Signup() {
     }
 
-    public boolean setUserID (String userID, String password, TreeMap type)
-    {
-        if (!type.containsKey(userID))
-        {
-            type.put(userID, password);
-            return true;
+    public void createNewUser(TreeMap<String, String> tree) throws IOException {
+        Input in = new Input();
+        userID = in.userID();
+        while (tree.containsKey(userID)) {
+            System.out.println("User ID already exists, Please enter some other User ID");
+            userID = br.readLine();
+            continue;
         }
-        return false;
+        System.out.println("Enter User Name");
+        userName = br.readLine(); 
+        password = in.password();
     }
 
+    public int checkSignUp() throws IOException {
+        int val = Integer.parseInt(br.readLine());
+        while (val != 1 && val != 2) {
+            System.out.println("Invalid Input, Please try again");
+            val = Integer.parseInt(br.readLine());
+            continue;
+        }
+        return val;
+    }
     // public boolean
-    
+
 }

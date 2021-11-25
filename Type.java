f86c83d1b2e9182abd18714b2ba6c8cdc02664ce
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 10:14:22 +0530

    Update Type.java

--- Type.java
+++ Type.java
@@ -1,14 +1,25 @@
 import java.io.*;
+import java.util.TreeMap;
 
 public class Type {
 
-    public int post() throws IOException
-    {
-        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
-        Details details = new Details();
-        details.loginDetails();
-        int ch = Integer.parseInt(br.readLine());
-        return ch;
+    public TreeMap <String, String>  post() throws IOException {
+        TreeMap <String, String> tree = new TreeMap<>();
+        while (true) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            Details details = new Details();
+            details.loginDetails();
+            int ch = Integer.parseInt(br.readLine());
+            if (ch == 1 || ch == 2 || ch == 3) {
+                MapReturn mp = new MapReturn();
+                tree = mp.type(ch);
+                break;
+            } else {
+                details.invalidLoginDetails();
+            }
+
+        }
+        return tree;
     }
-    
+
 }

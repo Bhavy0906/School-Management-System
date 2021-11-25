3a4f0f92adcfdde4826b18ad3aaef67acbc962f8
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 21:10:35 +0530

    Update FileManagement.java

--- FileManagement.java
+++ FileManagement.java
@@ -1,10 +1,10 @@
 import java.io.*;
 import java.util.*;
 import com.opencsv.*;
-
+import com.opencsv.exceptions.CsvException;
 
 public class FileManagement {
-
+    public int row = -1;
     // The class will contain the basic code of file initiation and usage.
     // Just copy paste it to the relevant portions.
 
@@ -24,18 +24,18 @@ public class FileManagement {
         return "";
     }
 
-    //Admin class login details
+    // Admin class login details
     // final public void adminDetails ()throws IOException
     // {
-    //     Writer out = null;
-    //     String str = "Admin333031" + "\n" + "User@123";
-    //     try {
-    //         out = new FileWriter("Admin.txt");
-    //         out.write(str);
-    //     } catch (Exception e) {
-    //         System.err.println(e);
-    //     }
-    //     out.close();
+    // Writer out = null;
+    // String str = "Admin333031" + "\n" + "User@123";
+    // try {
+    // out = new FileWriter("Admin.txt");
+    // out.write(str);
+    // } catch (Exception e) {
+    // System.err.println(e);
+    // }
+    // out.close();
     // }
 
     // Will return the file name in which the basic details of the students and
@@ -76,15 +76,16 @@ public class FileManagement {
     }
 
     // Writes the basic details of the student into a .csv file
-    public void detailsInputStudent(String userID, String name, String[] course, int[] marks, int attendence, int choice) throws IOException {
+    public void detailsInputStudent(String userID, String name, String[] course, String[] marks, int attendence,
+            int choice) throws IOException {
         Writer out = null;
         double grades = 0;
         try {
             out = new BufferedWriter(new FileWriter(typeCSV(choice), true));
             out.write(userID + "," + name + "," + attendence + ",");
             for (int i = 0; i < 5; i++) {
-                out.write(course[i] + "," + Integer.toString(marks[i]) + ",");
-                grades = grades + marks[i];
+                out.write(course[i] + "," + marks[i] + ",");
+                grades = grades + Integer.parseInt(marks[i]);
             }
             grades = grades / 5;
             out.write(Double.toString(grades));
@@ -96,44 +97,33 @@ public class FileManagement {
         out.close();
     }
 
-    // public void studentInfoAppend (String userID, String course[], String marks[])throws IOException
-    // {
-    //     Writer out = null;
-    //     Reader input = null;
-    //     int row = 0;
-    //     try {
-    //         out = new BufferedWriter(new FileWriter("StudentInfo.csv", true));
-    //         input = new BufferedReader (new FileReader ("StudentInfo.csv"));
-    //         String str = "";
-    //         String columns [] = str.split(",");
-    //         while ((str = input.readLine()) != null)
-    //         {
-    //             row++;
-    //             int len = userID.length();
-    //             String sub = str.substring(0, len);
-    //             int index = str.indexOf(",", len);
-    //             if (sub.equals(userID))
-    //             {   
-    //                 int atten = Integer.parseInt(columns[2]);
-    //                 atten++;
-    //                 for (int i = 0; i < 5; i++)
-    //                 {
-    //                     6
-    //                 }
-    //             }
-    //         }
-    //     } catch (Exception e) {
-    //         //TODO: handle exception
-    //     }
-    // }
-
+    public void studentInfoAppend(String userID, String course, int marks) throws IOException {
+        int row = 0;
+        try {
+            String str = "";
+            String columns[] = str.split(",");
+            str = infoReader(userID, 1);
+            Index index = new Index();
+            int col = index.findIndex(columns, course);
+            updateCSV("StudentInfo.csv", marks, row, col + 1);
+            // while ((str = input.readLine()) != null)
+            // {
+            // row++;
+            // int len = userID.length();
+            // String sub = str.substring(0, len);
+            // int index = str.indexOf(",", len);
+            // if (sub.equals(userID))
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 
     // Writes the basic details of the teacher into a .csv file
     public void detailsInputTeacher(String userID, String name, String[] course, int choice) throws IOException {
         Writer out = null;
         try {
             out = new BufferedWriter(new FileWriter(typeCSV(choice), true));
-            out.write(userID + "," + name + ",");
+            out.write("\n" + userID + "," + name + ",");
             for (int i = 0; i < 3; i++) {
                 out.write(course[i] + ",");
             }
@@ -160,23 +150,25 @@ public class FileManagement {
         out.close();
     }
 
-    public String infoReader (String userID, int choice)throws IOException{
+    public String infoReader(String userID, int choice) throws IOException {
         BufferedReader input = null;
         String name = typeCSV(choice);
         String str = "";
         try {
             input = new BufferedReader(new FileReader(name));
-            while ((str = input.readLine()) != null)
-            {
+            while ((str = input.readLine()) != null) {
+                row++;
                 int len = userID.length();
                 String sub = str.substring(0, len);
                 if (sub.equals(userID))
-                return str;
+                    return str;
             }
+            input.close();
         } catch (Exception e) {
             System.err.println(e);
             input.close();
         }
+        row = -1;
         return "";
     }
 
@@ -187,15 +179,15 @@ public class FileManagement {
         try {
             input = new BufferedReader(new FileReader(name));
             while ((str = input.readLine()) != null) {
-                if (str.equals(userID)){
+                if (str.equals(userID)) {
                     String inp = input.readLine();
                     return inp;
-                }
-                else{
+                } else {
                     input.readLine();
                     continue;
-                }  
-            } input.close();
+                }
+            }
+            input.close();
         } catch (Exception e) {
             System.err.println(e);
             // input.close();
@@ -203,7 +195,34 @@ public class FileManagement {
         return "";
     }
 
-    public void detailsInputStudent(String id, String name, String[] course, String[] marks, String attendence) {
+    public void updateCSV(String fileToUpdate, int replace, int row, int col) throws IOException, CsvException {
+
+        
+
+        // Read existing file
+        try {
+            // File inputFile = new File("StudentInfo.csv");
+            FileReader filereader = new FileReader("StudentInfo.csv");
+            CSVReader reader = new CSVReaderBuilder(filereader).build();
+            List<String[]> csvBody = reader.readAll();
+            // get CSV row column and replace with by using row and column
+            csvBody.get(row)[col] = Integer.toString(replace);
+            reader.close();
+
+            // Write to CSV file which is open
+            CSVWriter writer = new CSVWriter(new FileWriter("StudentInfo.csv"));
+            writer.writeAll(csvBody);
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println(e);
+        }
+
     }
+
+    // public void detailsInputStudent(String id, String name, String[] course,
+    // String[] marks, String attendence) {
+    // }
 
 }

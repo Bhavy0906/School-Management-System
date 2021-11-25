e5988abe3fa3904c7a418eb43efb8c5683328727
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 23:24:05 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -7,22 +7,19 @@ public class Admin extends User {
     private String id;
 
     public Admin ()
-    {
-
-    }
+    {}
 
     public Admin (String name, String id)
     {
         this.name = name;
         this.id = id;
-        User.admin.put(id, name);
     }
 
     public void basicDetails ()
     {
         System.out.println("Hello Admin");
         System.out.println("Welcome to the School");
-        System.out.println("Enter 1 for making new records of Teacher, 2 for making new records of Student, 3 for adding a new course into the list of courses offered by the school, 4 for deleting a existing course");
+        System.out.println("Enter 1 for making new records of Teacher, 2 for making new records of Student");
     }    
 
     

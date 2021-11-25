36ef4b37d7e8d2d9ff5e09d72076641a58cc5f1a
Uday-Mittal-8860 <91343270+Uday-Mi@users.noreply.github.com>
2021-11-25 16:32:02 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -37,7 +37,7 @@ public class Student extends User {
     public void getDetails() throws IOException {
         FileManagement fm = new FileManagement();
         String str = fm.infoReader(id, 1);
-        int index[] = new int[14];
+        int index[] = new int[13];
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
@@ -48,12 +48,12 @@ public class Student extends User {
         }
         System.out.println("Name of the student is :- " + str.substring(index[0], index[1]));
         System.out.println("Identity number of the student is :- " + id);
-        for (int i = 2; i < 11; i++) {
+        for (int i = 2; i < 11; i = i + 2) {
             System.out.println("Course of the student and marks obtained in it are :- "
                     + str.substring(index[i], index[i + 1]) + " " + str.substring(index[i + 1], index[i + 2]));
         }
         System.out.println("The total attendence of the student is :- " + str.substring(index[1], index[2]));
-        System.out.println("The grades of the student is :- " + str.substring(index[12], index[13]));
+        System.out.println("The grades of the student is :- " + str.substring(index[12]));
     }
 
 }

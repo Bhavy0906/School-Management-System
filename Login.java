8eff5809214ae3659382e629c315215ba3d7c960
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 10:15:04 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -6,12 +6,38 @@ public class Login extends User{
     private String userID;
     private String password;
     static BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
-
+    private int num;
     public Login()
     {
         userID = "";
         password = "";
     }
+
+    void loginValueCheck(TreeMap<String,String> tree) throws IOException{
+        num = Integer.parseInt(br.readLine());
+        Details dtl = new Details();
+        while(num !=  1){
+            dtl.invalidLoginDetails();
+            dtl.loginDetails();
+            num = Integer.parseInt(br.readLine());
+            continue;
+        }
+            Input in = new Input();
+            userID = in.userID();
+            // while(!tree.containsValue(userID)){
+            //     System.out.println("Username Incorrect");
+            //     userID = in.userID();
+            // }
+            password = in.password();
+            boolean check = loginCheck(userID, password, tree);
+            while(!check){
+              dtl.incorrectPassword();
+              password = in.password();
+              check = loginCheck(userID, password, tree);
+            }
+            System.err.println("Login Successful");
+
+        }
     
     boolean loginCheck (String userID, String password, TreeMap<String, String> type)throws IOException
     { 

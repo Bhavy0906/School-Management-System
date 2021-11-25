566f983ecc62c03e2df8cb442cb2b5dbce375020
Uday-Mittal-8860 <91343270+Uday-Mi@users.noreply.github.com>
2021-11-26 00:52:58 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+
+import com.opencsv.exceptions.CsvException;
 
 public class Admin extends User {
 
@@ -22,5 +25,8 @@ public class Admin extends User {
         System.out.println("Enter 1 for making new records of Teacher, 2 for making new records of Student");
     }    
 
-    
+    static void delete(String userID, int choice) throws IOException, CsvException{
+        FileManagement fm = new FileManagement(); 
+        fm.deleteEntry(userID, choice);
+    }
 }

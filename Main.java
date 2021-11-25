4c75c4bce37ee496c4b8e828c0826f5d3c58a58f
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 10:15:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,51 +9,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         Details details = new Details();
-        //Details class displays the basic information and various print statements guiding the user through the code.
+        // Details class displays the basic information and various print statements
+        // guiding the user through the code.
 
         User user = new User();
-        //Contains information about about the school
+        // Contains information about about the school
 
         details.basicDisplay();
-        //Displays the front page of the school
-        details.enterInto();
-        //Asks the user whether he wants to log in or sign in the system.
+        // Displays the front page of the school
+        TreeMap<String, String> tree;
+        Type type = new Type();
+        tree = type.post();
 
         Signup signup = new Signup();
         Login login = new Login();
-        Type type = new Type();
 
-        int t = 0;
-        TreeMap<String, String> tree;
-        MapReturn mapReturn = new MapReturn();
-        Input input = new Input();
-        String userID = "";
-        String password = "";
-        int enter = 0;
+        if (tree == User.student) {
+            details.enterLogin();
+            login.loginValueCheck(tree);
 
-        do {
-            enter = Integer.parseInt(br.readLine());
-            t = type.post();
-            if (t == 1 || t == 2 || t == 3) {
-                tree = mapReturn.type(t);
-            } else {
-                details.invalidLoginDetails();
-                break;
+        } else if (tree == User.teacher) {
+            details.enterLogin();
+            login.loginValueCheck(tree);
+
+        } else if (tree == User.admin) {
+            details.enterLogin();
+            login.loginValueCheck(tree);
+
+            details.enterSignup();
+            int value = signup.checkSignUp();
+            if (value == 1) {
+                signup.createNewUser(User.student);
+                Student std = new Student(signup.userID, signup.userName);
+                std.getDetails();
             }
-            userID = input.userID();
-            password = input.password();
-            if (password != "") {
-                switch (enter) {
-                case 1:
-                    signup.setUserID(userID, password, tree);
-                    break;
-                case 2:
-                    login.loginCheck(userID, password, tree);
-                    break;
-                }
-            } else
-                break;
-        } while (false);
+            if (value == 2) {
+                signup.createNewUser(User.teacher);
+                Teacher tch = new Teacher(signup.userID, signup.userName);
+            }
+
+        }
 
     }
+    
+
 }

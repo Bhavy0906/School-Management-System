996d94894f5e3f9872857a05256b7ebc33e280a6
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 21:10:44 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,18 +24,11 @@ public class Main {
         tree = type.post();
 
         Signup signup = new Signup();
-        //Creates instance of the sign up class
+        // Creates instance of the sign up class
         Login login = new Login();
+        Teacher tch;
 
-        if (tree == User.student) {
-            details.enterLogin();
-            login.loginValueCheck(1);
-
-        } else if (tree == User.teacher) {
-            details.enterLogin();
-            login.loginValueCheck(2);
-
-        } else if (tree == User.admin) {
+         if (tree == User.admin) {
             details.enterLogin();
             login.loginValueCheck(3);
 
@@ -43,62 +36,79 @@ public class Main {
             int value = signup.checkSignUp();
             if (value == 1) {
                 signup.createNewUser(value);
-                Student std = new Student(signup.userID, signup.userName, signup.password);
+                String[] course = new String[5];
+                System.out.println("Enter the courses taken(5).Please enter every in a new line");
+                for (int i = 0; i < 5; i++) {
+                    String cr = br.readLine();
+                    course[i] = cr;
+                }
+                System.out.println("Signup Successful");
+                Student std = new Student(signup.userID, signup.userName, signup.password, course);
                 std.getDetails();
             }
             if (value == 2) {
                 signup.createNewUser(value);
-                Teacher tch = new Teacher(signup.userID, signup.userName, signup.password);
+                String[] course = new String[3];
+                System.out.println("Enter the courses taken(3).Please enter every in a new line");
+                for (int i = 0; i < 3; i++) {
+                    String cr = br.readLine();
+                    course[i] = cr;
+                }
+                System.out.println("Signup Successful");
+                tch = new Teacher(signup.userID, signup.userName, signup.password, course);
+                tch.setDetails("Bhavy0906", "Meow", 50);
             }
 
         }
+        else if (tree == User.student) {
+            details.enterLogin();
+            login.loginValueCheck(1);
 
+        } else if (tree == User.teacher) {
+            details.enterLogin();
+            login.loginValueCheck(2);
+            // tch.setDetails("Bhavy0906", "Meow", 50);
 
+        }
 
-
-
-        //Different Approach
-        //Will be added only if it works fine
+        // Different Approach
+        // Will be added only if it works fine
         // Outer:
         // do {
-        //     enter = Integer.parseInt(br.readLine());
-        //     t = type.post();
-        //     //post method defines whether user is a student or a teacher or admin.
-        //     if (t == 1 || t == 2 || t == 3) {
-        //         tree = mapReturn.type(t);
-        //     } else {
-        //         details.invalidLoginDetails();
-        //         break;
-        //     }
-        //     userID = input.userID();
-        //     password = input.password();
-        //     if (password != "") {
-        //         if(!login.loginCheck(userID, password, tree))                   
-        //             break Outer;
-        //         } else{
-        //         System.out.println("Password format incorrect");
-        //         System.out.println("Try again");
-        //         break;
-        //     }
-        //     User object;
-        //     if (t == 1)
-        //     {
-        //         object = new Student ();
-        //     }
-        //     if (t == 2)
-        //     {
-        //         object = new Teacher ();
-        //     }
-        //     if (t == 3)
-        //     {
-        //         object = new Admin ();
-        //     }
-            
-
-
+        // enter = Integer.parseInt(br.readLine());
+        // t = type.post();
+        // //post method defines whether user is a student or a teacher or admin.
+        // if (t == 1 || t == 2 || t == 3) {
+        // tree = mapReturn.type(t);
+        // } else {
+        // details.invalidLoginDetails();
+        // break;
+        // }
+        // userID = input.userID();
+        // password = input.password();
+        // if (password != "") {
+        // if(!login.loginCheck(userID, password, tree))
+        // break Outer;
+        // } else{
+        // System.out.println("Password format incorrect");
+        // System.out.println("Try again");
+        // break;
+        // }
+        // User object;
+        // if (t == 1)
+        // {
+        // object = new Student ();
+        // }
+        // if (t == 2)
+        // {
+        // object = new Teacher ();
+        // }
+        // if (t == 3)
+        // {
+        // object = new Admin ();
+        // }
 
         // } while (false);
     }
-    
 
 }

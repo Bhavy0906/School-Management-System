351a22f7a38e230f7cd8fa8cb53370e9ff1c2f59
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-11-25 21:10:38 +0530

    Create Index.java

---
+++ Index.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Index {
+
+    // Linear-search function to find the index of an element
+    public int findIndex(String arr[], String t) {
+
+        // if array is Null
+        if (arr == null) {
+            return -1;
+        }
+
+        // find length of array
+        int len = arr.length;
+        int i = 0;
+
+        // traverse in the array
+        while (i < len) {
+
+            // if the i-th element is t
+            // then return the index
+            if (arr[i].equals(t)) {
+                return i;
+            } else {
+                i = i + 1;
+            }
+        }
+        return -1;
+    }
+}
